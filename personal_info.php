419ddba26a40c41c9f996aed355a7717c6b21391
Alvarobyui <u701860138@byui.edu>
2023-07-24 01:48:32 -0500

    personal info, implemented

--- personal_info.php
+++ personal_info.php
@@ -34,7 +34,7 @@
           </section>
           <section class="content">
             <p>PHOTO</p>
-            <div class="content--img"><img src="img.png" alt="profilephoto"></div>
+            <div class="content--img"><img src="./img/user.png" alt="profilephoto"></div>
           </section>
           <section class="content">
             <p>NAME</p>
@@ -65,9 +65,13 @@
     </main>
     <aside class="menu-desplegable">
       <div class="profile">
-        <div class="img"><img src="profile-photo.png" alt="phpto"></div>
+        <div class="img"><img src="./img/user.png" alt="phpto"></div>
         <p>Alvaro Diaz</p>
-        <span><</span>
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+          </svg>
+        </span>
       </div>
       <div class="menu">
         <button class="menu--btn">
